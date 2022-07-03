2f789f303914e816f91be426857e6db4069727d3
Hussayn Abdulrahmon <haroworld91@gmail.com>
2022-07-03 09:34:01 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens:{
+      sm:'480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors:{
+        lightBlue:'#0099cc',
+        darkBlue:'#105369',
+        lightGreen:'rgb(28, 163, 28);'
+      }
+    },
+  },
+  plugins: [],
+}
